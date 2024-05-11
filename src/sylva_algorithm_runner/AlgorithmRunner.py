02e39ccffa4bfad3d4e2a8fe7773e3651cd52f53
Robert Gebauer <robert.gebauer@gmx.de>
2024-05-11 20:56:06 +0200

    Build and clean run image.

--- src/sylva_algorithm_runner/AlgorithmRunner.py
+++ src/sylva_algorithm_runner/AlgorithmRunner.py
@@ -8,11 +8,13 @@ class AlgorithmRunner:
     configuration = None
     pid = None
     algorithm_docker_image_name = None
+    run_docker_image_name = None
 
     def __init__(self, configuration, pid):
         self.configuration = configuration    
         self.pid = pid
         self.algorithm_docker_image_name = f"{self.pid}-algorithm:latest"
+        self.run_docker_image_name = f"{self.pid}-run:latest"
 
     def run(self, algorithm_run_order: AlgorithmRunOrder):
         folder_path = os.path.join(self.configuration["path"], self.pid)
@@ -31,10 +33,25 @@ class AlgorithmRunner:
         print(stdout)
         print(stderr)
 
+        with open('/var/lib/sylva-algorithm-runner/Dockerfile.template', 'r') as file:
+            dockerfile_template = file.read()
+
+        dockerfile_content = dockerfile_template.replace('{{pid}}', self.pid)
+
+        (stdout, stderr) = subprocess.Popen(["docker", "build", "-t", self.run_docker_image_name, "-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, universal_newlines=True).communicate(
+            input=dockerfile_content
+        )
+        print(stdout)
+        print(stderr)
+
     def clean(self):
         folder_path = os.path.join(self.configuration["path"], self.pid)
         shutil.rmtree(folder_path)
 
         (stdout, stderr) = subprocess.Popen(["docker", "rmi", self.algorithm_docker_image_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, universal_newlines=True).communicate()
+        print(stdout)
+        print(stderr)
+
+        (stdout, stderr) = subprocess.Popen(["docker", "rmi", self.run_docker_image_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, universal_newlines=True).communicate()
         print(stdout)
         print(stderr)
