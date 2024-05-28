34aeadf824c258fc22ae074f8ad0530347be29a0
Robert Gebauer <robert.gebauer@gmx.de>
2024-05-28 20:45:08 +0200

    Cleanup.

--- src/sylva_algorithm_runner/AlgorithmRunner.py
+++ src/sylva_algorithm_runner/AlgorithmRunner.py
@@ -21,8 +21,10 @@ class RunSection(Enum):
 
 class Status(Enum):
     """ Enum for the different statuses of processes. """
+    RUNNING = "RUNNING"
     SUCCESS = "SUCCESS"
     FAILURE = "FAILURE"
+    WAITING_FOR_DATA = "WAITING_FOR_DATA"
 
 
 class AlgorithmRunner:
@@ -49,7 +51,7 @@ class AlgorithmRunner:
 
         os.makedirs(self.working_dir)
 
-        self.log_repository.start_run(self.pid)
+        self.log_repository.start_run()
 
         try:
             # Clone algorithm from foreign repository
@@ -57,18 +59,14 @@ class AlgorithmRunner:
             section_success = self.__run_and_log_section(RunSection.CLONE, clone_command)
             
             if not section_success:
-                self.__clean()
-                self.log_repository.end_run(self.pid, Status.FAILURE)
-                return
+                raise Exception()
             
             # Build algorithm docker image
             docker_build_algorithm_image = ["docker", "build", "-t", self.algorithm_docker_image_name, "."]
             section_success = self.__run_and_log_section(RunSection.BUILD_ALGORITHM_IMAGE, docker_build_algorithm_image)
             
             if not section_success:
-                self.__clean()
-                self.log_repository.end_run(self.pid, Status.FAILURE)
-                return
+                raise Exception()
             
             # Build run docker image (based on algorithm docker image + own extension to make it work); Dockerfile for this is part of this package
             with open('/var/lib/sylva-algorithm-runner/Dockerfile.template', 'r') as file:
@@ -85,9 +83,7 @@ class AlgorithmRunner:
                     section_success = self.__run_and_log_section(RunSection.BUILD_ALGORITHM_RUN_IMAGE, docker_build_algorithm_run_image)
 
             if not section_success:
-                self.__clean()
-                self.log_repository.end_run(self.pid, Status.FAILURE)
-                return
+                raise Exception()
             
             # Run the algorithm
             docker_run = ["docker", "run", "-d", "--name", "algorithm_container", self.run_docker_image_name]
@@ -110,13 +106,13 @@ class AlgorithmRunner:
 
             # last step: clean-up
             section_success = self.__clean()
-            self.log_repository.end_run(self.pid, Status.SUCCESS if section_success else Status.FAILURE)
+            self.log_repository.end_run(Status.SUCCESS if section_success else Status.FAILURE)
             
             return section_success
         
         except:
             self.__clean()
-            self.log_repository.end_run(self.pid, Status.FAILURE)
+            self.log_repository.end_run(Status.FAILURE)
 
 
     def __clean(self):
@@ -137,17 +133,17 @@ class AlgorithmRunner:
 
     def __run_and_log_section(self, run_section: RunSection, command: list) -> bool:
         """ Runs a command and logs the output to the given run_section in database. """
-        self.log_repository.start_section(self.pid, run_section)
+        self.log_repository.start_section(run_section)
         
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, universal_newlines=True, cwd=self.working_dir, bufsize=1)
 
         for line in iter(process.stdout.readline, ""):
-            self.log_repository.append_log(self.pid, run_section, line)      
+            self.log_repository.append_log(run_section, line)      
 
         process.communicate()
         
-        self.log_repository.set_section_status(self.pid, run_section, Status.SUCCESS if process.returncode == 0 else Status.FAILURE)
+        self.log_repository.set_section_status(run_section, Status.SUCCESS if process.returncode == 0 else Status.FAILURE)
 
-        self.log_repository.end_section(self.pid, run_section)
+        self.log_repository.end_section(run_section)
 
         return process.returncode == 0
--- src/sylva_algorithm_runner/repositories/LogRepository.py
+++ src/sylva_algorithm_runner/repositories/LogRepository.py
@@ -6,54 +6,63 @@ class LogRepository:
     configuration = None
     mongo_client = None
     log_to_stdout = False
+    pid = None
 
-    def __init__(self, database_configuration: dict, log_to_stdout: bool = False) -> None:
+    def __init__(self, database_configuration: dict, pid: str, log_to_stdout: bool = False) -> None:
         self.configuration = database_configuration
         self.mongo_client = MongoClient(self.configuration["host"], port=self.configuration["port"], username=self.configuration["user"], password=self.configuration["password"], authSource="admin", tz_aware=True)
         self.log_to_stdout = log_to_stdout
+        self.pid = pid
 
     def __get_algorithm_runs_collection(self):
         return self.mongo_client.sylva.algorithmRuns
 
-    def start_run(self, pid):
+    def start_run(self):
         algorithm_runs_collection = self.__get_algorithm_runs_collection()
-        algorithm_runs_collection.insert_one({"pid": pid, "start": datetime.now(timezone.utc)})
+        algorithm_runs_collection.insert_one({"pid": self.pid, "start": datetime.now(timezone.utc), "status": "RUNNING"})
 
-    def end_run(self, pid, status: object):
+    def end_run(self, status: object):
         algorithm_runs_collection = self.__get_algorithm_runs_collection()
         algorithm_runs_collection.update_one(
-            {"pid": pid},
+            {"pid": self.pid},
             {"$set": {"end": datetime.now(timezone.utc), "status": status.value}}
         )
 
-    def append_log(self, pid: str, run_section: object, log_line: str):
+    def append_log(self, run_section: object, log_line: str):
         if (self.log_to_stdout):
             print(log_line)
         
         algorithm_runs_collection = self.__get_algorithm_runs_collection()
         algorithm_runs_collection.update_one(
-            {"pid": pid },
+            {"pid": self.pid },
             {"$push": {f"sections.{run_section.value}.log": {"timestamp": datetime.now(timezone.utc), "output": log_line}}}
         )
 
-    def start_section(self, pid: str, run_section: object):
+    def log_workspace_id(self, workspace_id: str):
         algorithm_runs_collection = self.__get_algorithm_runs_collection()
         algorithm_runs_collection.update_one(
-            {"pid": pid},
+            {"pid": self.pid},
+            {"$set": {"workspace": workspace_id}}
+        )
+
+    def start_section(self, run_section: object):
+        algorithm_runs_collection = self.__get_algorithm_runs_collection()
+        algorithm_runs_collection.update_one(
+            {"pid": self.pid},
             {"$set": {f"sections.{run_section.value}.start": datetime.now(timezone.utc)}},
             upsert=True
         )
     
-    def end_section(self, pid: str, run_section: object):
+    def end_section(self, run_section: object):
         algorithm_runs_collection = self.__get_algorithm_runs_collection()
         algorithm_runs_collection.update_one(
-            {"pid": pid},
+            {"pid": self.pid},
             {"$set": {f"sections.{run_section.value}.end": datetime.now(timezone.utc)}}
         )
 
-    def set_section_status(self, pid: str, run_section: object, status: object):
+    def set_section_status(self, run_section: object, status: object):
         algorithm_runs_collection = self.__get_algorithm_runs_collection()
         algorithm_runs_collection.update_one(
-            {"pid": pid},
+            {"pid": self.pid},
             {"$set": {f"sections.{run_section.value}.status": status.value}}
         )
